a6b18e0ddbbb3101e4443356f452d86583991ee7
BuildTools <unconfigured@null.spigotmc.org>
2018-01-17 22:31:48 -0700

    Updates.

---
+++ src/main/java/com/pyxlhaus/SimVillages/Vector3.java
@@ -0,0 +1,16 @@
+package com.pyxlhaus.SimVillages;
+
+/**
+ * Created by Psi on 1/17/2018.
+ */
+public class Vector3 {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
