a059525c3fadcdb24c6cd6d699cefd7f7b1661e5
BuildTools <unconfigured@null.spigotmc.org>
2018-01-17 22:31:31 -0700

    Updates.

---
+++ src/main/java/com/pyxlhaus/SimVillages/Buildings/BuildingTemplate.java
@@ -0,0 +1,31 @@
+package com.pyxlhaus.SimVillages.Buildings;
+
+import com.google.common.primitives.UnsignedInteger;
+import com.pyxlhaus.SimVillages.Vector3;
+import org.bukkit.block.Block;
+
+import java.util.HashMap;
+
+/**
+ * Created by Psi on 1/17/2018.
+ */
+public class BuildingTemplate {
+    private HashMap<Integer, Block> building_blocks;
+    private Integer block_count;
+    private String creator;
+    private UnsignedInteger building_stage;
+    private String building_name;
+    private Vector3 max_dimensions;
+    public enum BuildingTypes{
+        RESIDENTIAL,
+        AGRICULTURAL,
+        RECREATIONAL,
+        COMMERCIAL,
+        INDUSTRIAL,
+        MARTIAL
+    }
+
+    public BuildingTemplate(){
+
+    }
+}
